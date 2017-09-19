f5b105e2802684df1b384a4ef13a7efc2e32a68f
Aran <chenxukunyx@hotmail.com>
2017-09-19 09:24:38 +0800

    ------

--- libs/src/main/java/com/miracle/libs/MiRacleActivity.java
+++ libs/src/main/java/com/miracle/libs/MiRacleActivity.java
@@ -12,6 +12,13 @@ import com.miracle.libs.utils.MLog;
 import com.miracle.libs.utils.ToastUtils;
 import com.miracle.libs.view.SuccessFailView;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+
 
 /**
  * Create with Android studio
@@ -55,6 +62,7 @@ public class MiRacleActivity extends Activity implements View.OnClickListener{
     public void onClick(View v) {
         if (v.getId() == R.id.loading) {
             successFailView.loadingStatus();
+            MLog.i("aran", test());
         } else if (R.id.success == v.getId()) {
             successFailView.successStatus();
         } else {
@@ -62,46 +70,52 @@ public class MiRacleActivity extends Activity implements View.OnClickListener{
         }
     }
 
-//    private void doSomeWork() {
-//        Observable observable = Observable.create(new ObservableOnSubscribe() {
-//            @Override
-//            public void subscribe(ObservableEmitter e) throws Exception {
-//                e.onNext(1);
-//                e.onNext(2);
-//                e.onNext("3");
-//                e.onComplete();
-//            }
-//        });
-//
-//        Observer observer = new Observer() {
-//            @Override
-//            public void onSubscribe(Disposable d) {
-//                MLog.i("onSubscribe : " + d.isDisposed());
-//            }
-//
-//            @Override
-//            public void onNext(Object value) {
-//                if (value instanceof String) {
-//                    MLog.i("onNext : " + value);
-//                }
-//            }
-//
-//            @Override
-//            public void onError(Throwable e) {
-//
-//            }
-//
-//            @Override
-//            public void onComplete() {
-//                MLog.i("onComplete");
-//            }
-//        };
-//
-//        observable.map(new Function() {
-//            @Override
-//            public Object apply(Object o) throws Exception {
-//                return o.toString();
-//            }
-//        }).subscribe(observer);
-//    }
+    private boolean test() {
+        Integer a = new Integer(30);
+        Integer b = new Integer(30);
+        return a == b;
+    }
+
+    private void doSomeWork() {
+        Observable observable = Observable.create(new ObservableOnSubscribe() {
+            @Override
+            public void subscribe(ObservableEmitter e) throws Exception {
+                e.onNext(1);
+                e.onNext(2);
+                e.onNext("3");
+                e.onComplete();
+            }
+        });
+
+        Observer observer = new Observer() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                MLog.i("onSubscribe : " + d.isDisposed());
+            }
+
+            @Override
+            public void onNext(Object value) {
+                if (value instanceof String) {
+                    MLog.i("onNext : " + value);
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+                MLog.i("onComplete");
+            }
+        };
+
+        observable.map(new Function() {
+            @Override
+            public Object apply(Object o) throws Exception {
+                return o.toString();
+            }
+        }).subscribe(observer);
+    }
 }
